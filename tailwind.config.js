c4a265ff9225430c6adf9ae13b3f32ee29ff1a13
FaridFadilah <faridfadillah29@gmail.com>
2022-11-16 19:47:18 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    container:{
+      center : true,
+      screens: {
+        xl: "1170px"
+      },
+    },
+    extend: {
+      colors:{
+        "myBlue" : "#217BF4" 
+      },
+      fontFamily: {
+        'inter': ['Inter', "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
